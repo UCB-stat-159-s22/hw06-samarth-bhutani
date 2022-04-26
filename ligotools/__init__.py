828c8f1fabd7b65f26b760e13260ac640b947de5
Samarth Bhutani <samarth.bhutani@berkeley.edu>
2022-04-26 01:08:23 +0000

    fixed package setup

---
+++ ligotools/__init__.py
@@ -0,0 +1,7 @@
+"""
+The ligo package.
+"""
+
+__version__ = "0.0.1"
+
+from .readligo import *
---
+++ ligotools/tests/.ipynb_checkpoints/test_readligo-checkpoint.py
@@ -0,0 +1,13 @@
+from ligotools import *
+
+def test1():
+    assert True
+
+def test2():
+    assert True
+    
+def test3():
+    assert True
+    
+def test4():
+    assert True
